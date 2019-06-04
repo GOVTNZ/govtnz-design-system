997e427ec4005d7b05e46bf9abc5616e02789ed5
David Craig <david@davidcraig.co.nz>
2019-06-04 12:38:18 +1200

    DS-242: Add 'focusable' attribute to Icon component

--- packages/govtnz-ds-website/src/components/Icon.tsx
+++ packages/govtnz-ds-website/src/components/Icon.tsx
@@ -4,15 +4,20 @@ import './icon.scss'
 type Props = {
   className?: string | 'icon'
   role: 'presentation' | 'img'
+  focusable: 'true' | 'false'
   id: string
   title?: string
 }
 
-const Icon = ({ className, role, id, title }: Props) => (
-  <svg className={className} role={role}>
+const Icon = ({ className, role, focusable, id, title }: Props) => (
+  <svg className={className} role={role} focusable={focusable}>
     {title && <title>{title}</title>}
     <use xlinkHref={`#${id}`} />
   </svg>
 )
+
+Icon.defaultProps = {
+  focusable: 'false',
+};
 
 export default Icon
