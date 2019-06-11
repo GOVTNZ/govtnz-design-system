0a63788dece46b017d3b8a5b31731be534fd8b99
David Craig <david@davidcraig.co.nz>
2019-06-11 13:14:11 +1200

    DS-279: Add docs for Button component

--- packages/govtnz-ds-website/src/docs/components/Button.md
+++ packages/govtnz-ds-website/src/docs/components/Button.md
@@ -1,126 +1,153 @@
 <P styleSize="large">
     Buttons make important actions stand out within a task flow, and help users
-    to do that action easily. Colours, labels or icons are used on buttons to
-    guide users about the action that will happen when they touch or
-    click the button.
+    to take that action easily. Use colour and text  in buttons to inform users
+    about the action that will happen when they touch or click a button.
 </P>
-
-## Examples
-
-Interact with each example to see how it works.
-
-<ExampleContainer>
-    <Example>
-        <Button type="Button">Save and continue</Button>
-    </Example>
-</ExampleContainer>
 
 ## When to use it
 
 Use buttons to help users carry out key actions in a task flow, like starting
 an application or saving their information.
 
+If you are considering using an icon in a button, we suggest you use visible
+text as well to ensure all users understand the button's action.
+
 ## How it works
 
-Write button labels in sentence case, describing the action it performs.
-For example:
+Keep button text short. Start with a verb that clearly describes the action the
+button performs. Write button text in sentence case.
+
+Some typical examples of button text are:
 
 * 'Start now' at the start of the service
 * 'Sign in' to an account a user has already created
 * 'Continue' when the service does not save a user's information
 * 'Save and continue' when the service does save a user's information
-* 'Save and come back later' when a user can save their information and
-come back later
-* 'Add another' to add another item to a list or group
+* 'Save and return later' when a user can save their information and come back
+to it later
 * 'Pay' to make a payment
 * 'Confirm and send' on a check answers page that does not have any legal
 content a user must agree to
-* 'Accept and send' on a check answers page that has legal content a user
-must agree to
-* 'Sign out' when a user is leaving an account that they signed in to.
+* 'Accept and send' on a check answers page that has legal content a user must
+agree to
+* 'Sign out' when a user is leaving an account that they signed in to
+* 'Add another [name of thing]' to add another item to a list or group. Ensure
+the button text describes clearly what this button will add, in case there are
+other similar buttons on the same page or form.
 
-You may need to include more or different words to clearly describe a button's
+Consider if you need to include more words to clearly describe a button's
 action. For example, 'Add another address' or 'Accept and claim a tax refund'.
 
-Align the primary action button to the left edge of your form.
+## Primary buttons
+
+Use a primary button for the main 'call to action' on a page, such as 'Save and
+send'. Avoid using several primary buttons on a single page. Having more than
+one 'call to action' reduces its impact, and makes it harder for users to know
+what to do next.
 
 <ExampleContainer>
-    <ExampleHeading>Default buttons</ExampleHeading>
+    <ExampleHeading>Primary buttons</ExampleHeading>
     <Example>
         <Button type="Button">Save and continue</Button>
     </Example>
 </ExampleContainer>
 
-Use a default button for the main 'call to action' on a page.
+## Secondary buttons
 
-Avoid using multiple default buttons on a single page. Having more than one
-main call to action reduces their impact, and makes it harder for users to
-know what to do next.
+Use secondary buttons for alternative or intermediary actions on a page.
+
+Pages with too many calls to action make it hard for users to know what to do
+next. Before adding a secondary button, try to simplify the page or break the
+content down into several pages.
 
 <ExampleContainer>
     <ExampleHeading>Secondary buttons</ExampleHeading>
     <Example>
-        <Button type="Button" kind="secondary">Find address</Button>
+        <Button type="Button" level="secondary">Find address</Button>
     </Example>
 </ExampleContainer>
 
-Use secondary buttons for secondary calls to action on a page.
-
-Pages with too many calls to action make it hard for users to know what to do
-next. Before adding lots of secondary buttons, try to simplify the page or
-break the content down into several pages.
+You can use secondary buttons in combination with a primary button, such as
+offering 'Cancel' or 'Save and return later' options. Consider limiting
+combinations to a maximum of two secondary buttons with a single primary
+button, as more can confuse users.
 
 <ExampleContainer>
     <ExampleHeading>Primary and secondary combination</ExampleHeading>
     <Example>
         <Button type="Button">Save and continue</Button>
-        <Button type="Button" kind="secondary">Save as draft</Button>
+        <Button type="Button" level="secondary">Save as draft</Button>
     </Example>
 </ExampleContainer>
 
-You can use secondary buttons in combination with default buttons. Limit
-combinations to two buttons only, as more can confuse users.
+## Warning buttons
+
+Warning buttons are designed to make users think carefully before they use them.
+They only work if used very sparingly.
+
+Only use warning buttons for actions with serious destructive consequences that
+cannot be easily undone by a user, such as permanently deleting an account.
+
+When letting users carry out an action like this, it's a good idea to include an
+extra first step that asks users to confirm they want to do
+the destructive action.
+
+In this instance, use one warning button for the initial call to action, and
+another warning button for the final confirmation.
+
+Do not only rely solely on the red colour of a warning button to communicate
+the serious nature of the action. Not all users will be able to see the colour
+or may not understand what it signifies. Make sure the context and button text
+make clear what will happen if the user selects it.
 
 <ExampleContainer>
     <ExampleHeading>Warning buttons</ExampleHeading>
     <Example>
-        <Button type="Button" kind="warning">Delete account</Button>
+        <Button type="Button" level="warning">Delete account</Button>
     </Example>
 </ExampleContainer>
 
-Warning buttons are designed to make users think carefully before they use
-them. They only work if used very sparingly. Most services should
-not need one.
+## 'Disabled' buttons
 
-Only use warning buttons for actions with serious destructive consequences
-that cannot be easily undone by a user. For example, permanently
-deleting an account.
+'Disabled' buttons, which are often greyed out, have poor contrast and can
+confuse some users, so avoid them if possible. 'Disabled' buttons may also
+impede keyboard accessibility.
 
-When letting users carry out an action like this, it's a good idea to include
-an additional step beforehand that asks users to confirm they want to do
-the destructive action.
-
-In this instance, use another style of button for the initial call to action,
-and a warning button for the final confirmation.
-
-Do not only rely solely on the red colour of a warning button to communicate
-the serious nature of the action. This is because not all users will be able to
-see the colour or may not understand what it signifies. Make sure the context
-and button text make clear what will happen if the user selects it.
+Only use 'disabled' buttons if your research shows it makes the user interface
+easier to understand.
 
 <ExampleContainer>
-    <ExampleHeading>Disabled buttons</ExampleHeading>
+    <ExampleHeading>'Disabled' buttons</ExampleHeading>
     <Example>
         <Button type="Button" disabled>Save and continue</Button>
     </Example>
 </ExampleContainer>
 
-Disabled buttons have poor contrast and can confuse some users, so avoid
-them if possible.
+## Stop users from accidentally sending information twice
 
-Only use disabled buttons if research shows it makes the user interface
-easier to understand.
+You can prevent users from accidentally sending information more than once by
+preventing multiple clicks from registering.
+
+Sometimes, users will click buttons more than once because they:
+
+* have used operating systems where they have to rapidly click twice on items
+to make them work
+* are experiencing a slow connection, which means they are not given feedback
+on their action quickly enough
+* have motor impairments, such as hand tremors, that cause them to click the
+button involuntarily.
+
+In some cases, multiple clicks can mean users' information is sent more than
+once. For example, a notify team found that a number of users were receiving
+invitations twice, because the person sending them was clicking twice on
+the 'Send' button.
+
+If you are working in production and your research shows that users are
+frequently sending information twice, consider using features that configure
+the button to ignore a rapid second click. If the cause is a slow connection,
+give the user a 'loading spinner' to show progress.
 
 ## Credit
 
-Guidance, original HTML and CSS derived from [GOV.UK Design System](https://github.com/alphagov/govuk-frontend).
+Guidance, original HTML and CSS derived from
+[GOV.UK Design System](https://github.com/alphagov/govuk-frontend).
--- packages/govtnz-ds-website/src/pages/components/Button.tsx
+++ packages/govtnz-ds-website/src/pages/components/Button.tsx
@@ -12,12 +12,12 @@ import P from '@govtnz/ds/build/react-ts/P.tsx';
 import '@govtnz/ds/build/css/P.css';
 import H2 from '@govtnz/ds/build/react-ts/H2.tsx';
 import '@govtnz/ds/build/css/H2.css';
-import Button from '@govtnz/ds/build/react-ts/Button.tsx';
-import '@govtnz/ds/build/css/Button.css';
 import Ul from '@govtnz/ds/build/react-ts/Ul.tsx';
 import '@govtnz/ds/build/css/Ul.css';
 import Li from '@govtnz/ds/build/react-ts/Li.tsx';
 import '@govtnz/ds/build/css/Li.css';
+import Button from '@govtnz/ds/build/react-ts/Button.tsx';
+import '@govtnz/ds/build/css/Button.css';
 import components__Button from '../../commons/examples/components__Button';
 // Indirect relative import because this template is output to src/pages/components so it needs to step back to `commons`.
 import ComponentPage from '../../commons/component-page';
@@ -29,110 +29,131 @@ import ExampleSection from '../../commons/ExampleSection';
 
 const PageContent = (props) => <React.Fragment><P styleSize="large">
     Buttons make important actions stand out within a task flow, and help users
-    to do that action easily. Colours, labels or icons are used on buttons to
-    guide users about the action that will happen when they touch or
-    click the button.
+    to take that action easily. Use colour and text  in buttons to inform users
+    about the action that will happen when they touch or click a button.
 </P>
 
-<H2 {...onChangeGenerator(H2)} styleSize="large" id="examples">Examples</H2>
-<p>Interact with each example to see how it works.</p>
-<ExampleContainer {...onChangeGenerator(ExampleContainer)}>
-    <Example {...onChangeGenerator(Example)} code={components__Button[0]} >
-        <Button {...onChangeGenerator(Button)} type="Button">Save and continue</Button>
-    </Example>
-</ExampleContainer>
-
-<h2 id="when-to-use-it">When to use it</h2>
+<H2 {...onChangeGenerator(H2)} styleSize="large" id="when-to-use-it">When to use it</H2>
 <p>Use buttons to help users carry out key actions in a task flow, like starting
 an application or saving their information.</p>
+<p>If you are considering using an icon in a button, we suggest you use visible
+text as well to ensure all users understand the button&#39;s action.</p>
 <h2 id="how-it-works">How it works</h2>
-<p>Write button labels in sentence case, describing the action it performs.
-For example:</p>
+<p>Keep button text short. Start with a verb that clearly describes the action the
+button performs. Write button text in sentence case.</p>
+<p>Some typical examples of button text are:</p>
 <Ul {...onChangeGenerator(Ul)} bulleted>
 <Li {...onChangeGenerator(Li)}>&#39;Start now&#39; at the start of the service</Li>
 <Li {...onChangeGenerator(Li)}>&#39;Sign in&#39; to an account a user has already created</Li>
 <Li {...onChangeGenerator(Li)}>&#39;Continue&#39; when the service does not save a user&#39;s information</Li>
 <Li {...onChangeGenerator(Li)}>&#39;Save and continue&#39; when the service does save a user&#39;s information</Li>
-<Li {...onChangeGenerator(Li)}>&#39;Save and come back later&#39; when a user can save their information and
-come back later</Li>
-<Li {...onChangeGenerator(Li)}>&#39;Add another&#39; to add another item to a list or group</Li>
+<Li {...onChangeGenerator(Li)}>&#39;Save and return later&#39; when a user can save their information and come back
+to it later</Li>
 <Li {...onChangeGenerator(Li)}>&#39;Pay&#39; to make a payment</Li>
 <Li {...onChangeGenerator(Li)}>&#39;Confirm and send&#39; on a check answers page that does not have any legal
 content a user must agree to</Li>
-<Li {...onChangeGenerator(Li)}>&#39;Accept and send&#39; on a check answers page that has legal content a user
-must agree to</Li>
-<Li {...onChangeGenerator(Li)}>&#39;Sign out&#39; when a user is leaving an account that they signed in to.</Li>
+<Li {...onChangeGenerator(Li)}>&#39;Accept and send&#39; on a check answers page that has legal content a user must
+agree to</Li>
+<Li {...onChangeGenerator(Li)}>&#39;Sign out&#39; when a user is leaving an account that they signed in to</Li>
+<Li {...onChangeGenerator(Li)}>&#39;Add another [name of thing]&#39; to add another item to a list or group. Ensure
+the button text describes clearly what this button will add, in case there are
+other similar buttons on the same page or form.</Li>
 </Ul>
-<p>You may need to include more or different words to clearly describe a button&#39;s
+<p>Consider if you need to include more words to clearly describe a button&#39;s
 action. For example, &#39;Add another address&#39; or &#39;Accept and claim a tax refund&#39;.</p>
-<p>Align the primary action button to the left edge of your form.</p>
+<h2 id="primary-buttons">Primary buttons</h2>
+<p>Use a primary button for the main &#39;call to action&#39; on a page, such as &#39;Save and
+send&#39;. Avoid using several primary buttons on a single page. Having more than
+one &#39;call to action&#39; reduces its impact, and makes it harder for users to know
+what to do next.</p>
 <ExampleContainer {...onChangeGenerator(ExampleContainer)}>
-    <ExampleHeading {...onChangeGenerator(ExampleHeading)}>Default buttons</ExampleHeading>
-    <Example {...onChangeGenerator(Example)} code={components__Button[1]} >
+    <ExampleHeading {...onChangeGenerator(ExampleHeading)}>Primary buttons</ExampleHeading>
+    <Example {...onChangeGenerator(Example)} code={components__Button[0]} >
         <Button {...onChangeGenerator(Button)} type="Button">Save and continue</Button>
     </Example>
 </ExampleContainer>
 
-<p>Use a default button for the main &#39;call to action&#39; on a page.</p>
-<p>Avoid using multiple default buttons on a single page. Having more than one
-main call to action reduces their impact, and makes it harder for users to
-know what to do next.</p>
+<h2 id="secondary-buttons">Secondary buttons</h2>
+<p>Use secondary buttons for alternative or intermediary actions on a page.</p>
+<p>Pages with too many calls to action make it hard for users to know what to do
+next. Before adding a secondary button, try to simplify the page or break the
+content down into several pages.</p>
 <ExampleContainer {...onChangeGenerator(ExampleContainer)}>
     <ExampleHeading {...onChangeGenerator(ExampleHeading)}>Secondary buttons</ExampleHeading>
-    <Example {...onChangeGenerator(Example)} code={components__Button[2]} >
-        <Button {...onChangeGenerator(Button)} type="Button" kind="secondary">Find address</Button>
+    <Example {...onChangeGenerator(Example)} code={components__Button[1]} >
+        <Button {...onChangeGenerator(Button)} type="Button" level="secondary">Find address</Button>
     </Example>
 </ExampleContainer>
 
-<p>Use secondary buttons for secondary calls to action on a page.</p>
-<p>Pages with too many calls to action make it hard for users to know what to do
-next. Before adding lots of secondary buttons, try to simplify the page or
-break the content down into several pages.</p>
+<p>You can use secondary buttons in combination with a primary button, such as
+offering &#39;Cancel&#39; or &#39;Save and return later&#39; options. Consider limiting
+combinations to a maximum of two secondary buttons with a single primary
+button, as more can confuse users.</p>
 <ExampleContainer {...onChangeGenerator(ExampleContainer)}>
     <ExampleHeading {...onChangeGenerator(ExampleHeading)}>Primary and secondary combination</ExampleHeading>
-    <Example {...onChangeGenerator(Example)} code={components__Button[3]} >
+    <Example {...onChangeGenerator(Example)} code={components__Button[2]} >
         <Button {...onChangeGenerator(Button)} type="Button">Save and continue</Button>
-        <Button {...onChangeGenerator(Button)} type="Button" kind="secondary">Save as draft</Button>
+        <Button {...onChangeGenerator(Button)} type="Button" level="secondary">Save as draft</Button>
     </Example>
 </ExampleContainer>
 
-<p>You can use secondary buttons in combination with default buttons. Limit
-combinations to two buttons only, as more can confuse users.</p>
+<h2 id="warning-buttons">Warning buttons</h2>
+<p>Warning buttons are designed to make users think carefully before they use them.
+They only work if used very sparingly.</p>
+<p>Only use warning buttons for actions with serious destructive consequences that
+cannot be easily undone by a user, such as permanently deleting an account.</p>
+<p>When letting users carry out an action like this, it&#39;s a good idea to include an
+extra first step that asks users to confirm they want to do
+the destructive action.</p>
+<p>In this instance, use one warning button for the initial call to action, and
+another warning button for the final confirmation.</p>
+<p>Do not only rely solely on the red colour of a warning button to communicate
+the serious nature of the action. Not all users will be able to see the colour
+or may not understand what it signifies. Make sure the context and button text
+make clear what will happen if the user selects it.</p>
 <ExampleContainer {...onChangeGenerator(ExampleContainer)}>
     <ExampleHeading {...onChangeGenerator(ExampleHeading)}>Warning buttons</ExampleHeading>
-    <Example {...onChangeGenerator(Example)} code={components__Button[4]} >
-        <Button {...onChangeGenerator(Button)} type="Button" kind="warning">Delete account</Button>
+    <Example {...onChangeGenerator(Example)} code={components__Button[3]} >
+        <Button {...onChangeGenerator(Button)} type="Button" level="warning">Delete account</Button>
     </Example>
 </ExampleContainer>
 
-<p>Warning buttons are designed to make users think carefully before they use
-them. They only work if used very sparingly. Most services should
-not need one.</p>
-<p>Only use warning buttons for actions with serious destructive consequences
-that cannot be easily undone by a user. For example, permanently
-deleting an account.</p>
-<p>When letting users carry out an action like this, it&#39;s a good idea to include
-an additional step beforehand that asks users to confirm they want to do
-the destructive action.</p>
-<p>In this instance, use another style of button for the initial call to action,
-and a warning button for the final confirmation.</p>
-<p>Do not only rely solely on the red colour of a warning button to communicate
-the serious nature of the action. This is because not all users will be able to
-see the colour or may not understand what it signifies. Make sure the context
-and button text make clear what will happen if the user selects it.</p>
+<h2 id="disabled-buttons">&#39;Disabled&#39; buttons</h2>
+<p>&#39;Disabled&#39; buttons, which are often greyed out, have poor contrast and can
+confuse some users, so avoid them if possible. &#39;Disabled&#39; buttons may also
+impede keyboard accessibility.</p>
+<p>Only use &#39;disabled&#39; buttons if your research shows it makes the user interface
+easier to understand.</p>
 <ExampleContainer {...onChangeGenerator(ExampleContainer)}>
-    <ExampleHeading {...onChangeGenerator(ExampleHeading)}>Disabled buttons</ExampleHeading>
-    <Example {...onChangeGenerator(Example)} code={components__Button[5]} >
+    <ExampleHeading {...onChangeGenerator(ExampleHeading)}>'Disabled' buttons</ExampleHeading>
+    <Example {...onChangeGenerator(Example)} code={components__Button[4]} >
         <Button {...onChangeGenerator(Button)} type="Button" disabled>Save and continue</Button>
     </Example>
 </ExampleContainer>
 
-<p>Disabled buttons have poor contrast and can confuse some users, so avoid
-them if possible.</p>
-<p>Only use disabled buttons if research shows it makes the user interface
-easier to understand.</p>
+<h2 id="stop-users-from-accidentally-sending-information-twice">Stop users from accidentally sending information twice</h2>
+<p>You can prevent users from accidentally sending information more than once by
+preventing multiple clicks from registering.</p>
+<p>Sometimes, users will click buttons more than once because they:</p>
+<Ul {...onChangeGenerator(Ul)} bulleted>
+<Li {...onChangeGenerator(Li)}>have used operating systems where they have to rapidly click twice on items
+to make them work</Li>
+<Li {...onChangeGenerator(Li)}>are experiencing a slow connection, which means they are not given feedback
+on their action quickly enough</Li>
+<Li {...onChangeGenerator(Li)}>have motor impairments, such as hand tremors, that cause them to click the
+button involuntarily.</Li>
+</Ul>
+<p>In some cases, multiple clicks can mean users&#39; information is sent more than
+once. For example, a notify team found that a number of users were receiving
+invitations twice, because the person sending them was clicking twice on
+the &#39;Send&#39; button.</p>
+<p>If you are working in production and your research shows that users are
+frequently sending information twice, consider using features that configure
+the button to ignore a rapid second click. If the cause is a slow connection,
+give the user a &#39;loading spinner&#39; to show progress.</p>
 <h2 id="credit">Credit</h2>
-<p>Guidance, original HTML and CSS derived from <a href="https://github.com/alphagov/govuk-frontend">GOV.UK Design System</a>.</p>
+<p>Guidance, original HTML and CSS derived from
+<a href="https://github.com/alphagov/govuk-frontend">GOV.UK Design System</a>.</p>
 </React.Fragment>
 
 export default function Code(props) {
