adf9e034b53f8f9dd065eb1a3a2535f8b803bb75
Janzen Zarzoso <janzen.zarzoso@gmail.com>
2019-11-27 12:03:09 +1300

    Added animation for skip-link.

--- packages/govtnz-ds-website/src/components/SkipLink.tsx
+++ packages/govtnz-ds-website/src/components/SkipLink.tsx
@@ -8,12 +8,24 @@ type SkipLinkProps = {
 const SkipLink = ({ href }: SkipLinkProps): JSX.Element => {
   const handleClick = e => {
     e.preventDefault();
-    // e.target.href includes the domain e.g. `http://localhost#main-content
-    // so getAttribute() is more appropriate here.
+
+    const reduceMotionQuery = '(prefers-reduced-motion: reduce)';
+    const hasOSReducedMotion = window.matchMedia(reduceMotionQuery).matches;
+
     const href = e.target.getAttribute('href').replace(/#/g, '');
-    document.getElementById(href).scrollIntoView({
-      behavior: 'smooth',
-    });
+
+    console.log('hasOSReducedMotion', hasOSReducedMotion);
+    const focusableElement = document.getElementById(href);
+    if (hasOSReducedMotion) {
+      focusableElement.setAttribute('tabindex', '-1');
+      focusableElement.focus();
+    } else {
+      // e.target.href includes the domain e.g. `http://localhost#main-content
+      // so getAttribute() is more appropriate here.
+      focusableElement.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
   };
 
   return (
