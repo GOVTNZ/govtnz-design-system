e22404b88dbbe46c9f40f8ca33399d80dd8571d1
Matthew Holloway <matthew@springload.co.nz>
2020-07-13 14:21:13 +1200

    Fixes today

--- packages/govtnz-ds/build/react-js-styled-components/Ul.js
+++ packages/govtnz-ds/build/react-js-styled-components/Ul.js
@@ -14,7 +14,7 @@ var _react = _interopRequireDefault(require("react"));
 var _styledComponents = _interopRequireDefault(require("styled-components"));
 
 function _templateObject4() {
-  var data = (0, _taggedTemplateLiteral2["default"])(["\n    list-style: none;\n    padding-left: 0px;\n    margin-left: 0px;\n  "]);
+  var data = (0, _taggedTemplateLiteral2["default"])(["\n      list-style: none;\n      padding-left: 0px;\n      margin-left: 0px;\n    "]);
 
   _templateObject4 = function _templateObject4() {
     return data;
@@ -24,7 +24,7 @@ function _templateObject4() {
 }
 
 function _templateObject3() {
-  var data = (0, _taggedTemplateLiteral2["default"])(["\n    margin-bottom: 0;\n    line-height: 32px;\n  "]);
+  var data = (0, _taggedTemplateLiteral2["default"])(["\n      margin-bottom: 0px !important;\n    "]);
 
   _templateObject3 = function _templateObject3() {
     return data;
@@ -34,7 +34,7 @@ function _templateObject3() {
 }
 
 function _templateObject2() {
-  var data = (0, _taggedTemplateLiteral2["default"])(["\n    margin-bottom: 0px !important;\n  "]);
+  var data = (0, _taggedTemplateLiteral2["default"])(["\n      margin-bottom: 0;\n      line-height: 32px;\n    "]);
 
   _templateObject2 = function _templateObject2() {
     return data;
@@ -44,7 +44,7 @@ function _templateObject2() {
 }
 
 function _templateObject() {
-  var data = (0, _taggedTemplateLiteral2["default"])(["font-family: g-theme-font-family;\n-webkit-font-smoothing: antialiased;\n-moz-osx-font-smoothing: grayscale;\nfont-weight: 400;\nfont-size: 1.25rem;\nline-height: 1.625;\ncolor: g-theme-color;\nmargin-top: 0;\nmargin-bottom: 1rem;\npadding-left: 20px;\nlist-style-type: disc;\n@media print{\nfont-family: sans-serif;;\n}\n@media (min-width: 40.0625em){\nfont-size: 1.1875rem;\nline-height: 1.31579;\nmargin-bottom: 1rem;;\n}\n@media print{\nfont-size: 14pt;\nline-height: 1.15;\ncolor: g-theme-print-color;;\n}\nmargin-top: 10px;\nmargin-top: 0.5rem;\n", "\n", "\n", ""]);
+  var data = (0, _taggedTemplateLiteral2["default"])(["\n  font-family: g-theme-font-family;\n  -webkit-font-smoothing: antialiased;\n  -moz-osx-font-smoothing: grayscale;\n  font-weight: 400;\n  font-size: 1.25rem;\n  line-height: 1.625;\n  color: g-theme-color;\n  margin-top: 0;\n  margin-bottom: 1rem;\n  padding-left: 20px;\n  list-style-type: disc;\n  ", "\n  @media print {\n    font-family: sans-serif;\n    font-size: 14pt;\n    line-height: 1.15;\n    color: g-theme-print-color;\n  }\n  @media (min-width: 40.0625em) {\n    font-size: 1.1875rem;\n    line-height: 1.31579;\n    margin-bottom: 1rem;\n    margin-bottom: 0;\n    line-height: 32px;\n  }\n  ", "\n  ", "\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -54,9 +54,9 @@ function _templateObject() {
 }
 
 var StyledUl = _styledComponents["default"].ul(_templateObject(), function (props) {
-  return props.marginBottom0 && _styledComponents["default"].css(_templateObject2());
+  return props.spacing && _styledComponents["default"].css(_templateObject2());
 }, function (props) {
-  return props.spacing && _styledComponents["default"].css(_templateObject3());
+  return props.marginBottom0 && _styledComponents["default"].css(_templateObject3());
 }, function (props) {
   return props.noBullet && _styledComponents["default"].css(_templateObject4());
 });
