b44dfc2d5c5374e324063347c164f7c3de15063b
Juliet Brown <juliet.brown@springload.co.nz>
2020-07-29 16:21:36 +1200

    remove redundant code

--- packages/govtnz-ds-website/src/commons/ExampleHeading.tsx
+++ packages/govtnz-ds-website/src/commons/ExampleHeading.tsx
@@ -11,20 +11,16 @@ export default ({ children, white, level }: Props) => {
   const Heading = `h${level !== undefined ? level : 4}`;
 
   return (
-    <>
-      <div
-        className={
-          children
-            ? `example__heading${white ? 'example__heading--white' : ''}`
-            : 'visually-hidden'
-        }
-      >
-        <Heading
-          className={'g-heading-m g-dswebsite-font example__heading-text'}
-        >
-          {children ? children : 'Example default'}
-        </Heading>
-      </div>
-    </>
+    <div
+      className={
+        children
+          ? `example__heading${white ? 'example__heading--white' : ''}`
+          : 'visually-hidden'
+      }
+    >
+      <Heading className={'g-heading-m g-dswebsite-font example__heading-text'}>
+        {children ? children : 'Example default'}
+      </Heading>
+    </div>
   );
 };
